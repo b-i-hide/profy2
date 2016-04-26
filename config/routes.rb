fc234d6eaa1fa9bc669b7ce8018cb35c5113a6a5
Hideaki Oshima <b.i1993patriots@gmail.com>
2016-04-26 13:34:06 +0900

    これまでの作業

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:show, :edit, :update]
+  root to: "top#index"
+  resources :groups, only: [:show, :edit, :update]
+  resources :questions, only: [:create]
+  resources :answers, only: [:new, :create]
+
+end
